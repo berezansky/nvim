0a68903fba32fe72db05ef49a67dc178b4f7fd1e
Илья Березанский <i.berezansky97@gmail.com>
2023-02-01 00:17:55 +0600

    show hidden files by default

--- lua/plugins/configs/neo-tree.lua
+++ lua/plugins/configs/neo-tree.lua
@@ -1,6 +1,6 @@
-require'keys.alias'
+require 'keys.alias'
 
-local neotree = require'neo-tree'
+local neotree = require 'neo-tree'
 
 vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
 
@@ -8,18 +8,27 @@ nm('<leader>nf', '<cmd>Neotree filesystem<cr>')
 nm('<leader>nb', '<cmd>Neotree buffers<cr>')
 nm('<leader>ng', '<cmd>Neotree git_status<cr>')
 
-neotree.setup{
-  popup_border_style = "rounded",
-  enable_git_status = true,
-  enable_diagnostics = true,
-  window = {
-    position = 'float',
-    mapping_options = {
-      noremap = true,
-      nowait = true,
+neotree.setup {
+    popup_border_style = "rounded",
+    enable_git_status = true,
+    enable_diagnostics = true,
+    window = {
+        position = 'float',
+        mapping_options = {
+            noremap = true,
+            nowait = true,
+        },
+        mappings = {
+            ['o'] = 'open',
+        },
     },
-    mappings = {
-      ['o'] = 'open',
+    filesystem = {
+        filtered_items = {
+            visible = true,
+            hide_dotfiles = false,
+            hide_gitignored = false,
+            hide_hidden = false,
+        }
     }
-  }
+
 }
