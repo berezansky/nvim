6b636dd34020eeb55f4a92386a012f724df11343
berezansky <i.berezansky97@gmail.com>
2025-04-16 10:00:13 +0500

    add new keymaps

--- lua/keys/keymaps.lua
+++ lua/keys/keymaps.lua
@@ -8,6 +8,9 @@ nm('<C-k>', '<C-w><Up>')
 nm('<C-j>', '<C-w><Down>')
 nm('<C-l>', '<C-w><Right>')
 nm('<C-h>', '<C-w><Left>')
+nm('<leader>ef', '<cmd>EslintFixAll<CR>')
+nm('<leader>cs', '<cmd>colorscheme solarized<CR>')
+nm('<leader>ct', '<cmd>colorscheme tokyonight-storm<CR>')
 
 -- Clear Search
 nm('//', '<cmd>:noh<cr>')
